79ce153913e79d81e0b55202b56d4cbd2f97478a
miketwheeler <mikeytwheeler@gmail.com>
2021-04-25 23:27:35 -0700

    added in static files and began events

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {getFeaturedEvents} from '../dummy-data';
+import EventList from '../components/events/event-list'
 function HomePage() {
+	const featuredEvents = getFeaturedEvents();
 	return (
 		<div>
-			<h1>The Home Page</h1>
+			<EventList items={featuredEvents} />
 		</div>
 	)
 }
